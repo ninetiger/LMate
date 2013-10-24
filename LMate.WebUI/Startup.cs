7ab095e53be14b6855267cb64c81f02e7d6d66a2
Xiao Gong <ninetiger@gmail.com>
2013-10-24 21:53:59 +1300

    updated .net to 4.5 create new empty webui project

---
+++ LMate.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LMate.WebUI.Startup))]
+namespace LMate.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

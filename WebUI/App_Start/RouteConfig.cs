052fb5c7b35c1a760465b8c8803d5cdafd93a734
Xiao.Gong <ninetiger@gmail.com>
2014-01-02 20:12:05 +1300

    Added Permissions controller and index page

--- WebUI/App_Start/RouteConfig.cs
+++ WebUI/App_Start/RouteConfig.cs
@@ -13,7 +13,7 @@ namespace WebUI
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-                defaults: new { controller = "Receipts", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
